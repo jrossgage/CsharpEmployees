c252f7b7587e9d40428f95a25ed1a9f666d39eb2
Joel Gage <jrossgage@gmail.com>
2021-05-27 09:22:57 -0500

    Completed exercise

---
+++ Employee.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Classes
+{
+    public class Employee
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public Employee(string firstName, string lastName, string title, DateTime date)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Title = title;
+            StartDate = date;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+            Company NSS = new Company("NSS", DateTime.Now);
+            // Create three employees
+            Employee Andy = new Employee("Andy", "Collins", "Teacher", DateTime.Now);
+            Employee Joe = new Employee("Joe", "Shepherd", "Lead Instructor", DateTime.Now);
+            Employee Bryan = new Employee("Good", "Bryan", "Abandonist", DateTime.Now);
+            // Assign the employees to the company
+            NSS.Employees.Add(Andy);
+            NSS.Employees.Add(Joe);
+            NSS.Employees.Add(Bryan);
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
+            NSS.ListEmployee();
+        }
+    }
+}
